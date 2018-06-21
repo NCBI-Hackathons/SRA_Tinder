6052b7660c524ccfc0b1676ce76459b18ba22704
Colin <colin.diesh@gmail.com>
2018-06-21 16:32:19 -0400

    Store quality in variable

--- process.sh
+++ process.sh
@@ -7,5 +7,6 @@ fastq-dump --split-files --defline-seq '@$ac.$si.$sg/$ri' --defline-qual '+' -Z 
 #cat $1 > python readfq.py
 fastqc $1.fq
 
-bioawk -c fastx '{ print ">"$name; print meanqual($qual) }' $1.fq|grep -v ">"|awk '{s+=$1}END{print "ave:",s/NR}' RS=" " > $1.meanqual
+qual=$(bioawk -c fastx '{ print ">"$name; print meanqual($qual) }' $1.fq|grep -v ">"|awk '{s+=$1}END{print "",s/NR}' RS=" ")
+echo "Found quality: $qual"
 
