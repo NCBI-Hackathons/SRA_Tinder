648d88e4baeeb5819598f354f13fc22dec87934e
Colin <colin.diesh@gmail.com>
2018-06-21 16:15:27 -0400

    Add mean quality scores

--- process.sh
+++ process.sh
@@ -1,3 +1,5 @@
 fastq-dump --split-files --defline-seq '@$ac.$si.$sg/$ri' --defline-qual '+' -Z $1 | tee $1.fq | python readfq.py
 fastqc $1.fq
 
+bioawk -c fastx '{ print ">"$name; print meanqual($qual) }' $1.fq|grep -v ">"|awk '{s+=$1}END{print "ave:",s/NR}' RS=" " > $1.meanqual
+
