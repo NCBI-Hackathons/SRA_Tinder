b61e31bcefd5a88a6435440f7a3879969e8dc48c
Rob Schaefer <rob@linkage.io>
2018-06-21 14:58:23 -0600

    add initial setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages, Extension
+import os
+
+import io
+import re
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+setup(
+    name = 'minus80',
+    version = find_version('src','__init__.py'),
+    packages = find_packages(),
+    scripts = [
+    ],
+    ext_modules = [],
+    cmdclass = {
+        #'build_ext': build_ext
+        #'develop': PostDevelopCommand,
+        #'install': PostInstallCommand,
+    },
+
+    package_data = {
+        #'':['*.cyx']    
+    },
+    install_requires = [		
+    ],
+    include_package_data=True,
+
+    author = '',
+    author_email = '',
+    description = '',
+    license = "",
+    url = ''
+)
