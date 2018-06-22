da9251e4ffdac76173ef5206da45782754a0edfd
maallen3 <Mary.A.Allen@colorado.edu>
2018-06-22 16:54:21 -0600

    pretty pictures

--- SRA_Tinder/iloveprettypictures.py
+++ SRA_Tinder/iloveprettypictures.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import plotly.plotly as py
 import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import sys
 
 
@@ -18,6 +19,15 @@ def profilepic(tablefile):
                'yaxis': {
                 'title': 'Marks '}
              }})
+def temp():
+    init_notebook_mode()
+    iplot([{'x': [1, 2, 3], 'y': [5, 2, 7]}])
+    # We can also download an image of the plot by setting the image to the
+    #format you want. e.g. `image='png'`
+    #iplot([{'x': [1, 2, 3], 'y': [5, 2, 7]}], image='png')
+
+temp()
+
 
 
 if __name__=="__main__":
