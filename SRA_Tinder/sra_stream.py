58dfbd8dffee3b1cc3e4b18a6a2da48bd3c3899c
Rob Schaefer <rob@linkage.io>
2018-06-22 10:39:24 -0600

    a one line implentation of SRA streaming

--- SRA_Tinder/sra_stream.py
+++ SRA_Tinder/sra_stream.py
@@ -11,54 +11,53 @@ from ngs.ReadCollection import ReadCollection
 from ngs.Read import Read
 from ngs.ReadIterator import ReadIterator
 
+def run(acc, splitNum=1, splitNo=1):
+    '''
+    This is a blocking task, it needs to be run in an executor
+    '''
+    # open requested accession using SRA implementation of the API
+    with NGS.openReadCollection(acc) as run:
+        run_name = run.getName()
+
+        # compute window to iterate through
+        MAX_ROW = run.getReadCount()
+        chunk = MAX_ROW/splitNum
+        first = int(round(chunk*(splitNo-1)))
+        next_first = int(round(chunk*(splitNo)))
+        if next_first > MAX_ROW:
+            next_first = MAX_ROW
+
+        # start iterator on reads
+        with run.getReadRange(first+1, next_first-first, Read.all) as it:
+            i = 0
+            while it.nextRead():
+                i += 1
+                if i > 20000: 
+                    break
+                while it.nextFragment():
+                    bases = it.getFragmentBases()
+                    qualities=it.getFragmentQualities()
+                    ids=it.getFragmentId()
+                    if bases:
+                        read = f'@{ids}\n{bases}\n+\n{qualities}\n'
+                        return read
+                        #print ("\t{} - {}".format(bases, "aligned" if it.isAligned() else "unaligned"))
+                        #print ('@'+ids) 
+                        #print (bases)
+                        #print ('+'+ids)
+                        #print (qualities)
+                        #print ("\n")
+            #print ("Read {} spots for {}".format(i,  run_name))
 
 class SRA_Stream():
 
     def __init__(self):
         pass
 
-    def run(acc, splitNum=1, splitNo=1):
-        '''
-        This is a blocking task, it needs to be run in an executor
-        '''
-        # open requested accession using SRA implementation of the API
-        with NGS.openReadCollection(acc) as run:
-            run_name = run.getName()
-
-            # compute window to iterate through
-            MAX_ROW = run.getReadCount()
-            chunk = MAX_ROW/splitNum
-            first = int(round(chunk*(splitNo-1)))
-            next_first = int(round(chunk*(splitNo)))
-            if next_first > MAX_ROW:
-                next_first = MAX_ROW
-
-            # start iterator on reads
-            with run.getReadRange(first+1, next_first-first, Read.all) as it:
-                i = 0
-                while it.nextRead():
-                    i += 1
-                    if i > 20000: 
-                        break
-                    while it.nextFragment():
-                        bases = it.getFragmentBases()
-                        qualities=it.getFragmentQualities()
-                        ids=it.getFragmentId()
-                        if bases:
-                            read = f'@{ids}\n{bases}\n+\n{qualities}\n'
-                            yield read
-                            #print ("\t{} - {}".format(bases, "aligned" if it.isAligned() else "unaligned"))
-                            #print ('@'+ids) 
-                            #print (bases)
-                            #print ('+'+ids)
-                            #print (qualities)
-                            #print ("\n")
-                #print ("Read {} spots for {}".format(i,  run_name))
-
-    async def pipe_reads(self,pipe):
-        loop = asyncio.get_event_loop()
-        async with aiofiles.open(pipe,mode='w') as pipe:
-            future = loop.run_in_executor(None, self.run, pipe)
+    async def pipe_reads(self,acc,loop):
+        #loop = asyncio.get_event_loop()
+        async with aiofiles.open(acc,mode='w') as pipe:
+            future = loop.run_in_executor(None, run, acc)
             data = await future 
             await pipe.write(str(data))
         return None
@@ -75,15 +74,20 @@ class SRA_Stream():
         ------
         Opens a named pipe
         '''
+        loop = asyncio.get_event_loop()
         tasks = []
         for run in runs:
             try:
                 os.unlink(run)
-            except Exception as e:
+            except FileNotFoundError as e:
                 pass
             os.mkfifo(run)
-            tasks.append(self.pipe_reads(run))
+            tasks.append(self.pipe_reads(run,loop))
         results = asyncio.gather(*tasks)
-        loop = asyncio.get_event_loop()
         loop.run_until_complete(results)
         return results
+
+
+if __name__ == '__main__':
+    x = SRA_Stream()
+    x.run(['SRR020192'])
